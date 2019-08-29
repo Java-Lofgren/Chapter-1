aaab15884110a22bd9e7f2571a13effd058e2b7b
Chayton <chaytonlofgren@gmail.com>
2019-08-29 09:30:47 -0500

    Completed Random Guess

--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -4,6 +4,7 @@ public class RandomGuess {
 
     public static void main (String[] args) {
 //        System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null, "Guess a number between 1 and 10");
         JOptionPane.showMessageDialog( null, "This number is " + (1+(int)(Math.random()*10)));
     }
 }
